66d9ba412e798ceb1bfb9107e554b6fbc01d9334
Anton Petrenko <apetrenko@expediagroup.com>
2026-05-20 19:13:38 +0200

    add created project

---
+++ ExchangeCalculator/ExchangeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeCalculatorApp.swift
+//  ExchangeCalculator
+//
+//  Created by Антон Петренко on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
